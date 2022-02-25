5cffd4bafe8d9f52f841908755d5408d7ba2aa41
e1601143 <e1601143@edu.vamk.fi>
2022-02-25 08:18:45 +0200

    for deploy

--- server/app.js
+++ server/app.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
-const port=8000;
+const port=process.env.PORT || 8000;
 const cors = require('cors');
 app.use(express.json());
 app.use(cors());
 //require('./db/db');
 app.use('/tuf/v1', require('./routes/routes'));
-
-
 app.listen(port);
 console.log('App Runs on '+port);
+
+// Accessing the path module
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
