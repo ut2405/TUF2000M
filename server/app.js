08e073f4ed2b0188f4118a8a7e08b3a87134b0cd
e1601143 <e1601143@edu.vamk.fi>
2022-02-25 09:28:03 +0200

    for deploy9

--- server/app.js
+++ server/app.js
@@ -10,12 +10,12 @@ app.use('/tuf/v1', require('./routes/routes'));
 app.listen(port);
 console.log('App Runs on '+port);
 
-if (process.env.NODE_ENV === 'production') {
+
   // Serve any static files
   app.use(express.static(path.join(__dirname, '../client/build')));
   // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
   });
-}
+
 
